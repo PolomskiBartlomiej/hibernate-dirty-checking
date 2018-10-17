879ea9aaf3b93d85fe38929e02b72a44eb2228e9
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-10-17 22:48:22 +0200

    Initial commit

---
+++ src/main/java/com/example/hibernatedirtychecking/HibernateDirtyCheckingApplication.java
@@ -0,0 +1,12 @@
+package com.example.hibernatedirtychecking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateDirtyCheckingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateDirtyCheckingApplication.class, args);
+    }
+}
