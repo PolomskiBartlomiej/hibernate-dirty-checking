561b8b8d13b2faaadfe959fdf0e28db41dc6fc51
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-10-21 19:22:12 +0200

    doc update

--- README.md
+++ README.md
@@ -33,3 +33,44 @@ entities. In short: immutable entities are not tracked by this mechanism.
 
 To get more information please refer my other project
 https://github.com/mtumilowicz/hibernate-immutable
+
+# project description
+We log all queries (`application.properties`):
+```
+spring.jpa.properties.hibernate.show_sql=true
+```
+and in `DirtyCheckTest` we perform operations to show how dirty-checking
+in hibernate works:
+* `noChange`
+    ```
+    noChange start
+    Hibernate: select customer0_.id as id1_0_0_, customer0_.name as name2_0_0_ from customer customer0_ where customer0_.id=?
+    Hibernate: select customer0_.id as id1_0_1_, customer0_.name as name2_0_1_, issues1_.issues_id as issues_i3_1_3_, issues1_.id as id1_1_3_, issues1_.id as id1_1_0_, issues1_.description as descript2_1_0_ from customer customer0_ left outer join issue issues1_ on customer0_.id=issues1_.issues_id where customer0_.id=?
+    noChange end
+    ```
+* `field_change`
+    ```
+    field_change start
+    Hibernate: select customer0_.id as id1_0_0_, customer0_.name as name2_0_0_ from customer customer0_ where customer0_.id=?
+    Hibernate: select customer0_.id as id1_0_1_, customer0_.name as name2_0_1_, issues1_.issues_id as issues_i3_1_3_, issues1_.id as id1_1_3_, issues1_.id as id1_1_0_, issues1_.description as descript2_1_0_ from customer customer0_ left outer join issue issues1_ on customer0_.id=issues1_.issues_id where customer0_.id=?
+    Hibernate: update customer set name=? where id=?
+    field_change end    
+    ```
+* `change_in_collection`
+    ```
+    change_in_collection start
+    Hibernate: select customer0_.id as id1_0_0_, customer0_.name as name2_0_0_, issues1_.issues_id as issues_i3_1_1_, issues1_.id as id1_1_1_, issues1_.id as id1_1_2_, issues1_.description as descript2_1_2_ from customer customer0_ left outer join issue issues1_ on customer0_.id=issues1_.issues_id where customer0_.id=?
+    Hibernate: select customer0_.id as id1_0_1_, customer0_.name as name2_0_1_, issues1_.issues_id as issues_i3_1_3_, issues1_.id as id1_1_3_, issues1_.id as id1_1_0_, issues1_.description as descript2_1_0_ from customer customer0_ left outer join issue issues1_ on customer0_.id=issues1_.issues_id where customer0_.id=?
+    Hibernate: select issue0_.id as id1_1_0_, issue0_.description as descript2_1_0_ from issue issue0_ where issue0_.id=?
+    Hibernate: insert into issue (description, id) values (?, ?)
+    Hibernate: update issue set issues_id=? where id=?
+    change_in_collection end    
+    ```
+* `field_change_in_relation`
+    ```
+    field_change_in_relation start
+    Hibernate: select customer0_.id as id1_0_0_, customer0_.name as name2_0_0_, issues1_.issues_id as issues_i3_1_1_, issues1_.id as id1_1_1_, issues1_.id as id1_1_2_, issues1_.description as descript2_1_2_ from customer customer0_ left outer join issue issues1_ on customer0_.id=issues1_.issues_id where customer0_.id=?
+    Hibernate: select customer0_.id as id1_0_1_, customer0_.name as name2_0_1_, issues1_.issues_id as issues_i3_1_3_, issues1_.id as id1_1_3_, issues1_.id as id1_1_0_, issues1_.description as descript2_1_0_ from customer customer0_ left outer join issue issues1_ on customer0_.id=issues1_.issues_id where customer0_.id=?
+    Hibernate: update issue set description=? where id=?
+    field_change_in_relation end    
+    ```
--- src/test/java/com/example/hibernatedirtychecking/DirtyCheckTest.java
+++ src/test/java/com/example/hibernatedirtychecking/DirtyCheckTest.java
@@ -50,11 +50,6 @@ public class DirtyCheckTest {
         
         repository.save(customer);
         System.out.println("noChange end");
-
-//        noChange start
-//        Hibernate: select customer0_.id as id1_0_0_, customer0_.name as name2_0_0_ from customer customer0_ where customer0_.id=?
-//        Hibernate: select customer0_.id as id1_0_1_, customer0_.name as name2_0_1_, issues1_.issues_id as issues_i3_1_3_, issues1_.id as id1_1_3_, issues1_.id as id1_1_0_, issues1_.description as descript2_1_0_ from customer customer0_ left outer join issue issues1_ on customer0_.id=issues1_.issues_id where customer0_.id=?
-//        noChange end
     }
 
     @Test
@@ -65,12 +60,6 @@ public class DirtyCheckTest {
         
         repository.save(customer);
         System.out.println("field_change end");
-
-//        field_change start
-//        Hibernate: select customer0_.id as id1_0_0_, customer0_.name as name2_0_0_ from customer customer0_ where customer0_.id=?
-//        Hibernate: select customer0_.id as id1_0_1_, customer0_.name as name2_0_1_, issues1_.issues_id as issues_i3_1_3_, issues1_.id as id1_1_3_, issues1_.id as id1_1_0_, issues1_.description as descript2_1_0_ from customer customer0_ left outer join issue issues1_ on customer0_.id=issues1_.issues_id where customer0_.id=?
-//        Hibernate: update customer set name=? where id=?
-//        field_change end
     }
     
     @Test
@@ -84,14 +73,6 @@ public class DirtyCheckTest {
 
         repository.save(customer);
         System.out.println("change_in_collection end");
-
-//        change_in_collection start
-//        Hibernate: select customer0_.id as id1_0_0_, customer0_.name as name2_0_0_, issues1_.issues_id as issues_i3_1_1_, issues1_.id as id1_1_1_, issues1_.id as id1_1_2_, issues1_.description as descript2_1_2_ from customer customer0_ left outer join issue issues1_ on customer0_.id=issues1_.issues_id where customer0_.id=?
-//        Hibernate: select customer0_.id as id1_0_1_, customer0_.name as name2_0_1_, issues1_.issues_id as issues_i3_1_3_, issues1_.id as id1_1_3_, issues1_.id as id1_1_0_, issues1_.description as descript2_1_0_ from customer customer0_ left outer join issue issues1_ on customer0_.id=issues1_.issues_id where customer0_.id=?
-//        Hibernate: select issue0_.id as id1_1_0_, issue0_.description as descript2_1_0_ from issue issue0_ where issue0_.id=?
-//        Hibernate: insert into issue (description, id) values (?, ?)
-//        Hibernate: update issue set issues_id=? where id=?
-//        change_in_collection end
     }
     
     @Test
@@ -102,11 +83,5 @@ public class DirtyCheckTest {
 
         repository.save(customer);
         System.out.println("field_change_in_relation end");
-
-//        field_change_in_relation start
-//        Hibernate: select customer0_.id as id1_0_0_, customer0_.name as name2_0_0_, issues1_.issues_id as issues_i3_1_1_, issues1_.id as id1_1_1_, issues1_.id as id1_1_2_, issues1_.description as descript2_1_2_ from customer customer0_ left outer join issue issues1_ on customer0_.id=issues1_.issues_id where customer0_.id=?
-//        Hibernate: select customer0_.id as id1_0_1_, customer0_.name as name2_0_1_, issues1_.issues_id as issues_i3_1_3_, issues1_.id as id1_1_3_, issues1_.id as id1_1_0_, issues1_.description as descript2_1_0_ from customer customer0_ left outer join issue issues1_ on customer0_.id=issues1_.issues_id where customer0_.id=?
-//        Hibernate: update issue set description=? where id=?
-//        field_change_in_relation end
     }
 }
